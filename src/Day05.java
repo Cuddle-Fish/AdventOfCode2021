ea510617ae97d627898efc34703b8b1cd1e9c4ae
Kurt <graefk@wwu.edu>
2021-12-05 23:54:08 -0800

    Complete Day 5 Part 2
    Changing Task to Part BECAUSE SOMEONE COMPLAINED

--- src/Day05.java
+++ src/Day05.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -7,23 +8,23 @@ public class Day05 {
 
     public static void main(String[] args) {
         File ventInput = new File("src\\Nearby_Hydrothermal_Vents.txt");
-//        File ventInput = new File("src\\Test.txt");
-        VentDiagram diagram = new VentDiagram(ventInput);
+        VentDiagram diagram = new VentDiagram(ventInput, false);
         System.out.println("Task 1: " + diagram.getTotalVentsAboveTwo());
+        VentDiagram diagram2 = new VentDiagram(ventInput, true);
+        System.out.println("Task 2: " + diagram2.getTotalVentsAboveTwo());
     }
-
 }
 
 
 class VentDiagram {
     HashMap<Integer, Columns> row;
 
-    public VentDiagram(File ventInput) {
+    public VentDiagram(File ventInput, boolean addDiagonal) {
         row = new HashMap<>();
-        readVentList(ventInput);
+        readVentList(ventInput, addDiagonal);
     }
 
-    public void readVentList(File ventInput) {
+    public void readVentList(File ventInput, boolean addDiagonal) {
         try {
             Scanner readVents = new Scanner(ventInput);
             readVents.useDelimiter(",| -> |\r\n");
@@ -36,16 +37,17 @@ class VentDiagram {
                 }
 
                 if (currentVent[0] == currentVent[2] || currentVent[1] == currentVent[3]) {
-                    addHorizontalRows(currentVent);
+                    addStraightRow(currentVent);
+                } else if (addDiagonal) {
+                    addDiagonal(currentVent);
                 }
-
             }
         } catch (FileNotFoundException e) {
             System.out.println("Error, file not found");
         }
     }
 
-    private void addHorizontalRows(int[] currentVent) {
+    private void addStraightRow(int[] currentVent) {
         // vertical vent
         if (currentVent[0] == currentVent[2]) {
             int startRow, endRow;
@@ -91,6 +93,45 @@ class VentDiagram {
         }
         for (int i = startColumn; i <= endColumn; i++) {
             this.row.get(row).addColumn(i);
+        }
+    }
+
+
+    public void addDiagonal(int[] currentVent) {
+        boolean incrementColumnUp = true;
+        boolean incrementRowUp = true;
+        if (currentVent[0] > currentVent[2]) {
+            incrementColumnUp = false;
+        }
+        if (currentVent[1] > currentVent[3]) {
+            incrementRowUp = false;
+        }
+
+        int currentColumn = currentVent[0];
+        int currentRow = currentVent[1];
+
+        while (true) {
+            if (!row.containsKey(currentRow)) {
+                Columns newColumn = new Columns();
+                row.put(currentRow, newColumn);
+            }
+            row.get(currentRow).addColumn(currentColumn);
+
+            if (incrementColumnUp) {
+                ++currentColumn;
+            } else {
+                --currentColumn;
+            }
+            if (incrementRowUp) {
+                ++currentRow;
+            } else {
+                --currentRow;
+            }
+
+            if (incrementColumnUp && currentColumn > currentVent[2]
+                    || !incrementColumnUp && currentColumn < currentVent[2]) {
+                break;
+            }
         }
     }
 
